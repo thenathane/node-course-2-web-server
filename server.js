076772b4a07e3b577f70fbeca8327e77a099d940
nathan esra <thenathanesra@gmail.com>
2019-01-28 12:10:38 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!<h1>');
+    res.render('home.hbs', {
+        pageTitle: 'The Home page',
+        welcomeMessage: 'Welcome to my site',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'The about page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/contact', (req, res) =>{
+    res.send({
+        name: 'Nathan',
+        contact: [
+            '+276 3176 4566',
+            'nathanesra@gmail.com'        
+        ]
+    });
+});
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        errorMessage: 'Unable to handle request',        
+    });
+});
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
+
